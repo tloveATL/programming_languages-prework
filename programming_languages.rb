7ec2957f742424b63615259c42d0e6819b3c75bd
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-17 21:17:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,5 +5,5 @@ def reformat_languages(languages)
       if new_hash[name]
         new_hash[name][:style] << type
       else
-        new_hash[name] = { :type => data[:type], 
+        new_hash[name] = { :type => data[:type], :style => 
 end
