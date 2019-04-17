32cfb2cbe68be0290d4f201bff2e5eff231537c0
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-17 21:12:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,5 @@ def reformat_languages(languages)
   new_hash = []
   languages.each do |type, language_stuff|
     language_stuff.each do |name, data|
-      data.each do |data_name, value|
-        if new_hash.key?(name) == false
-          
+      
 end
